8501170aa6807d05c673af65f717ff065591d0d0
mac-deep <75491406+mac-deep@users.noreply.github.com>
2021-11-07 12:16:26 +0530

    feat: ed sheeran video added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,20 @@ function App() {
           <Accordion title="graph">
             <Graph />
           </Accordion>
+          <Accordion title="Ed Sheeran on 10000 hour theory">
+            <div className="youtube_container">
+              <iframe
+                className="youtube_iframe"
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/RDyg_41QF1w"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </Accordion>
         </div>
       </div>
     </div>
